e94ef6a932c08cbca326f27a7ff42fe528636c6c
Chuqubick <47313553+Chuqubick@users.noreply.github.com>
2022-01-24 17:22:44 +0300

    Update geomap.js

--- geomap.js
+++ geomap.js
@@ -27,37 +27,15 @@
           this.addEventListener("click", event => {
             var event = new Event("onClick");
             this.dispatchEvent(event);  });
-	  this.render();
           this._props = {};
     }
     
     render(){
-     google.charts.load('current', { 'packages': ['map'], 'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY' }); 
-     google.charts.setOnLoadCallback(drawMap);
-    
-     function drawMap() {
-      	var data = google.visualization.arrayToDataTable([
-        ['Country', 'Population'],
-        ['China', 'China: 1,363,800,000'],
-        ['India', 'India: 1,242,620,000'],
-        ['US', 'US: 317,842,000'],
-        ['Indonesia', 'Indonesia: 247,424,598'],
-        ['Brazil', 'Brazil: 201,032,714'],
-        ['Pakistan', 'Pakistan: 186,134,000'],
-        ['Nigeria', 'Nigeria: 173,615,000'],
-        ['Bangladesh', 'Bangladesh: 152,518,015'],
-        ['Russia', 'Russia: 146,019,512'],
-        ['Japan', 'Japan: 127,120,000']
-      ]);
-
-    var options = {
-      showTooltip: true,
-      showInfoWindow: true
-    };
-
-    var map = new google.visualization.Map(document.getElementById('map'));
-
-    map.draw(data, options);
+     	import * as am4core from "@amcharts/amcharts4/core";
+	import * as am4maps from "@amcharts/amcharts4/maps";
+	import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+	    
+	let map = am4core.create("map", am4maps.MapChart);
     }
 	    
     onCustomWidgetBeforeUpdate(changedProperties) {
