48317ee691bbef4af143ec8fb5ee02fc81b52e06
Chuqubick <47313553+Chuqubick@users.noreply.github.com>
2022-01-24 17:03:12 +0300

    Update geomap.js

--- geomap.js
+++ geomap.js
@@ -12,14 +12,11 @@
 
 		body {
 			background: #fff;
-			
-			.map svg{
-		  		max-width: 100%;
-		  	}
+		 
+		 <div class="map"></div>
 		}
 		</style>
 		
-		<div class="map"> <svg viewBox="0 0 500 500"></svg></div>
     ';
 
     class GeoMap extends HTMLElement {
@@ -27,19 +24,40 @@
           let shadowRoot = this.attachShadow({mode: "open"});
           shadowRoot.appendChild(template.content.cloneNode(true));
           this.$style = shadowRoot.querySelector('style');
-          this.$svg = shadowRoot.querySelector('svg');
           this.addEventListener("click", event => {
             var event = new Event("onClick");
             this.dispatchEvent(event);  });
+	  this.render();
           this._props = {};
     }
     
     render(){
-    	var c = document.getElementById("map");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
-	ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-	ctx.stroke();
+     google.charts.load('current', { 'packages': ['map'], 'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY' }); 
+     google.charts.setOnLoadCallback(drawMap);
+    
+     function drawMap() {
+      	var data = google.visualization.arrayToDataTable([
+        ['Country', 'Population'],
+        ['China', 'China: 1,363,800,000'],
+        ['India', 'India: 1,242,620,000'],
+        ['US', 'US: 317,842,000'],
+        ['Indonesia', 'Indonesia: 247,424,598'],
+        ['Brazil', 'Brazil: 201,032,714'],
+        ['Pakistan', 'Pakistan: 186,134,000'],
+        ['Nigeria', 'Nigeria: 173,615,000'],
+        ['Bangladesh', 'Bangladesh: 152,518,015'],
+        ['Russia', 'Russia: 146,019,512'],
+        ['Japan', 'Japan: 127,120,000']
+      ]);
+
+    var options = {
+      showTooltip: true,
+      showInfoWindow: true
+    };
+
+    var map = new google.visualization.Map(document.getElementById('map'));
+
+    map.draw(data, options);
     }
 	    
     onCustomWidgetBeforeUpdate(changedProperties) {
