d8408c3dc51616fbfb282a59e494f0e2bd41e537
Chuqubick <47313553+Chuqubick@users.noreply.github.com>
2022-01-24 17:51:59 +0300

    Update geomap.js

--- geomap.js
+++ geomap.js
@@ -12,11 +12,16 @@
 
 		body {
 			background: #fff;
-		 
-		 <div class="map"></div>
+		 	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 		}
-		</style>
 		
+		#chartdiv { 
+			width: 100%;
+			height: 200px;
+  			margin: 1em;
+		}
+		<div id="chartdiv"></div>
+		</style>
     ';
 
     class GeoMap extends HTMLElement {
@@ -31,11 +36,37 @@
     }
     
     render(){
-     	import * as am4core from "https://cdn.amcharts.com/lib/4/core.js";
-	import * as am4maps from "https://cdn.amcharts.com/lib/4/maps.js";
-	import am4geodata_worldLow from "https://cdn.amcharts.com/lib/4/geodata/worldLow.js";
-	    
-	let map = am4core.create("map", am4maps.MapChart);
+	var chart = am4core.create("chartdiv", am4maps.MapChart);
+	chart.geodata = am4geodata_worldLow;
+	chart.projection = new am4maps.projections.Orthographic();
+	chart.deltaLatitude = -30;
+	chart.panBehavior = "rotateLongLat";
+	chart.marginTop = 20;
+	chart.marginBottom = 20;
+	
+	// Create map polygon series
+	var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+	polygonSeries.useGeodata = true;
+	polygonSeries.north = 90;
+	polygonSeries.south = -90;
+	
+	// Configure series
+	var polygonTemplate = polygonSeries.mapPolygons.template;
+	polygonTemplate.tooltipText = "{name}";
+	polygonTemplate.fill = am4core.color("#d7e7ce");
+	polygonTemplate.stroke = am4core.color("#aaa");
+	polygonTemplate.strokeWidth = 1;
+	
+	// Create hover state and set alternative fill color
+	var hs = polygonTemplate.states.create("hover");
+	hs.properties.fill = am4core.color("#367B25");
+	
+	var grid = chart.series.push(new am4maps.GraticuleSeries());
+	grid.toBack();
+	
+	chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#aadaff");
+	chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 1;
+	chart.backgroundSeries.toBack();
     }
 	    
     onCustomWidgetBeforeUpdate(changedProperties) {
